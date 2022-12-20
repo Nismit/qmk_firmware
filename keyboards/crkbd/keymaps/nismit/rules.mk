375536aa88dca6ae4f01ee60ca889be2f3cfcd0a
Nismit <nismit.dev@gmail.com>
2022-12-19 22:42:58 -0800

    v0.1.2

---
+++ keyboards/crkbd/keymaps/nismit/rules.mk
@@ -0,0 +1,7 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+RGBLIGHT_ENABLE = no # Enable RGB light
+RGB_MATRIX_ENABLE = yes
+MOUSEKEY_ENABLE = no # Reduce firmware size
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+SLEEP_LED_ENABLE = no
